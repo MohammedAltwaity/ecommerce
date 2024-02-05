41eb6e0f3cbbca448aeaf754ac20f4b03524bb62
Moahmmed <mohammed4777762455@gmail.com>
2024-02-05 19:21:24 +0100

    fixing

--- ecommerce-project-release-2.0/13-backend /src/main/java/com/mohammedaltwaity/ecommerce/dao/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mohammedaltwaity.ecommerce.dao;
-
-import com.mohammedaltwaity.ecommerce.entity.Customer;
-import lombok.Lombok;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
